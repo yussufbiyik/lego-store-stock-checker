2c689328fc0f20f979cce7c96c5f156a64943fdc
Yusuf Bıyık <yusufbyk3660@gmail.com>
2024-03-14 00:58:48 +0300

    Improve readability & Add readme.md

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const config = require('./config.json');
+const WATCHLIST = config.watchlist;
 const LEGO_STORE_LINK = config.legoStoreLink;
 const CHECK_INTERVAL = config.checkIntervalAsCron;
-const WATCHLIST = config.watchlist;
+
+function getKeychainInfoElement($, element) {
+    return $(element).find(".col.col-12.vitrin-product-name.detailLink").attr("title").split(" ");
+}
+
+function getKeychainCode(keychainInfoElement) {
+    return keychainInfoElement.shift();
+}
+
+function getKeychainName(keychainInfoElement) {
+    keychainInfoElement.pop(); // Remove "Anahtarlık" word
+    return keychainInfoElement.join(" ");
+}
+
+function getKeychainPrice($, element) {
+    return $(element).find(".vitrin-current-price.currentPrice").text().trim().replace("\n", " ") || "Fiyat Yok";
+}
 
 function checkKeychains(args) {
     const results = []
@@ -15,23 +32,23 @@ function checkKeychains(args) {
             const html = response.data;
             const $ = cheerio.load(html);
             // Find all keychains
-            const elements = $(".col.col-12.hover-box.drop-down.hover");
-            console.log(`${elements.length} anahtarlık bulundu, ${args.join(",")} aranıyor.`)
+            const keychains = $(".col.col-12.hover-box.drop-down.hover");
+            console.log(`${keychains.length} anahtarlık bulundu, ${args.join(",")} kodlu anahtarlıklar aranıyor.`)
             // Iterate over all keychains
-            elements.each((index, element) => {
+            keychains.each((index, keychain) => {
                 // Get the full name of the keychain and split it by spaces
-                const keychainFull = $(element).find(".col.col-12.vitrin-product-name.detailLink").attr("title").split(" ");
+                const keychainInfoElement = getKeychainInfoElement($, keychain);
                 // Remove the first element from the array and assign it to keychainCode
-                const keychainCode = keychainFull.shift();
+                const keychainCode = getKeychainCode(keychainInfoElement);
                 // If the keychain code is not in the arguments, skip this iteration
-                if(!args.includes(keychainCode)) return;;
-                const keyChainPrice = $(element).find(".vitrin-current-price.currentPrice").text().trim().replace("\n", " ") || "Fiyat Yok";
+                if(args[0] != 0 && !args.includes(keychainCode)) return;
+                const keyChainPrice = getKeychainPrice($, keychain) || "Fiyat Yok";
                 let isOutOfStock = false;
-                keychainFull.pop(); // Remove "Anahtarlık" word
-                const keychainName = keychainFull.join(" ");
+                keychainInfoElement.pop(); // Remove "Anahtarlık" word
+                const keychainName = keychainInfoElement.join(" ");
                 // Check if the keychain is in stocks, not being in stock, 
                 // not being in production are almost the same thing so there's no need to distinguish it.
-                if($(element).find('.out-of-stock').length != 0) isOutOfStock = true;
+                if($(keychain).find('.out-of-stock').length != 0) isOutOfStock = true;
                 results.push({ 
                     "Kod": keychainCode, 
                     "İsim": keychainName, 
@@ -50,7 +67,7 @@ function checkKeychains(args) {
 // Get arguments from cli
 const args = process.argv.slice(2);
 if(args.length === 0) {
-    console.log("Herhangi bir argüman verilmedi, program izleme listesi modunda çalışıyor.");
+    console.log("Herhangi bir argüman verilmedi, program izleme listesini kontrol etme modunda çalışıyor.");
     cron.schedule(CHECK_INTERVAL, () => {
         console.log('Checking for the keychains...');
         checkKeychains(WATCHLIST);
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Lego Store Stok Kontrolcüsü
+Bu proje, belirli Lego anahtarlıklarının stok durumunu kontrol etmek için oluşturulmuştur. Proje, belirli aralıklarla Lego Store Türkiye'nin web sitesini kontrol eder ve belirli anahtarlıkların stok durumunu kontrol eder.
+
+## Kurulum
+Projenin kurulumu için aşağıdaki adımları takip edebilirsiniz:
+
+1. Projeyi klonlayın veya indirin.
+2. Terminalde proje klasörüne gidin.
+3. ```npm install``` komutunu çalıştırın. projenin çalışması için gerekli olan paketler yüklenecektir.
+
+## Kullanım
+Proje, ```config.json``` dosyasında belirtilen ya da argüman olarak verilen anahtarlık kodlarını kontrol eder. Eğer kod olarak 0 girilirsee tüm ürünleri kontrol eder.
+
+Kontrol edilecek anahtarlıkların listesi config.json dosyasında watchlist olarak belirtilmiştir.
+
+Proje, belirli aralıklarla kontrol yapar. Bu aralık config.json dosyasında checkIntervalAsCron olarak belirtilmiştir ve cron formatında olmalıdır. Varsayılan olarak her 4 saatte bir çalışır.
+
+Proje, izleme listesini sürekli kontrol etmek için: 
+```bash
+$ npm start
+``` 
+komutu ile çalıştırılabilir.
+
+Eğer sadece belirli ürünlere bakmak istiyorsanız:
+```bash
+$ node index.js <ANAHTARLIK_KODU> <ANAHTARLIK_KODU>
+```
+şeklinde istediğiniz kadar anahtarlık kodu girilerek çalıştırılabilir. 
+
+## Lisans
+Bu proje ISC lisansı ile lisanslanmıştır.
+
+## Ekran Görüntüleri
+
+![Her ürünü kontrol ederse.](screenshots/image.png)
